ac3c1e3a6a6af1858d7ddf1239590cf0ad1d1d20
tuonghuynh11 <21520123@ms.uit.edu.vn>
2022-09-15 12:18:50 +0700

    Add new file

---
+++ SumArray/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class FirstProgram
+{
+    public static int Sum(int[] arr)
+    {
+        int sum = 0;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            sum += arr[i];
+        }
+        return sum;
+    }
+    public static void Main(string []args)
+    {
+        int n;
+        Console.WriteLine("Nhap so luong phan tu");
+
+        n = int.Parse(Console.ReadLine());
+        int[] arr = new int[n];
+        for (int i = 0; i <n; i++)
+        {
+            Console.WriteLine("Nhap phan tu thu "+(i+1));
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Tong phan tu trong mang = " + Sum(arr));
+
+    }
+}
+
