bbc309adf335e025bf2136dfa3b9f3f32a040e43
DNM03 <113103124+DNM03@users.noreply.github.com>
2022-09-15 12:38:00 +0700

    New branch (#2)
    * This is a new Branch
    
    * new file

---
+++ SumArray/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArray
+{
+    class Class1
+    {
+        public void Print()
+        {
+            Console.WriteLine("HI");
+        }
+    }
+
+}
